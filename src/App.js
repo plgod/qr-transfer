132ff523317ab0c33e104f159d5b285b2f37691a
Pierre-Luc Godin <plgodin@gmail.com>
2021-10-14 18:20:38 -0400

    Basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import QRCode from "react-qr-code";
+import "./App.css";
 
 function App() {
+  const [qr, setQr] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={qr} onChange={(event) => setQr(event.target.value)} />
+      <QRCode value={qr} />
     </div>
   );
 }
