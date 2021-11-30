ab09a2eb7499e192a460132e02d23732cae61fb3
Pierre-Luc Godin <pierre-luc.godin@hiloenergie.com>
2021-11-30 17:05:04 -0500

    Added subtext for loading spinner

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,7 +1,15 @@
 import "./Spinner.css";
 
 function Spinner() {
-  return <div className="loader"></div>;
+  return (
+    <>
+      <div className="loader"></div>
+      <p className="sub">
+        I cheaped out on cloud hosting so the first load will take about 30s.
+        Bear with me...
+      </p>
+    </>
+  );
 }
 
 export default Spinner;
